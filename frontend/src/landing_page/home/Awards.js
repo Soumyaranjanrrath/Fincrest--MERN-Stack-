3818af306dbabdfdc79d8e42b169d883734710ce
Soumyaranjanrrath <soumya2005rath@gmail.com>
2025-10-02 19:25:59 +0530

    Home-components

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React from "react";
 
 function Awards() {
-    return ( 
-        <h1>Awards</h1>
-    );
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-6 p-5">
+          <img src="media/images/largestBroker.svg" />
+        </div>
+        <div className="col-6 p-5 mt-5">
+          <h1>Award-winning technology. Trusted by traders.</h1>
+          <p className="mb-5">
+            Recognized by the industry for our cutting-edge platform and
+            unwavering client support.
+          </p>
+          <div className="row">
+            <div className="col-6 p-5">
+              <ul>
+                <li>
+                  <p>Future and Options</p>
+                </li>
+                <li>
+                  <p>Comodity derivatives</p>
+                </li>
+                <li>
+                  <p>Currency derivatives</p>
+                </li>
+              </ul>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>
+                  <p>Stocks & IPOs</p>
+                </li>
+                <li>
+                  <p>Direct mutual funds</p>
+                </li>
+                <li>
+                  <p>Bonds and Govt.securities</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <img src="media/images/pressLogos.png" alt="press-logos" style={{width:"95%"}} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Awards;
--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 function Hero() {
     return ( 
-        <h1>Hero</h1>
+        <div className='container p-5 mb-5'>
+            <div className='row text-center'>
+                <img src='media/images/HomeHero.png' alt='hero' className='mb-5' /> 
+                <h1 className='mt-5'>Invest in Everything</h1>
+                <p>Online platform to invest in stocks, derivatives, mutualfunds, and more.</p>
+                <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"18%",margin: "0 auto"}}>Signup Now</button>
+            </div>
+
+
+        </div>
     );
 }
 
