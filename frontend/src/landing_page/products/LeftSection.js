4cac8c4f246f72d62bb2be672bb57a9ea07d186b
Soumyaranjanrrath <soumya2005rath@gmail.com>
2026-02-04 23:17:34 +0530

    leftsection in product done

--- frontend/src/landing_page/products/LeftSection.js
+++ frontend/src/landing_page/products/LeftSection.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React from "react";
 
-function LeftSection() {
-    return (
-        <div className="container mb-5">
-            <div className="row">
-                <div className="col-md-6">
-                    <h2>Left Section</h2>
-                    <p>This is the left section of the products page.</p>
-                </div>
-                <div className="col-md-6">
-                    <img src="https://via.placeholder.com/500" alt="placeholder" className="img-fluid" />
-                </div>
-            </div>
+function LeftSection({
+  imageURL,
+  productName,
+  productDescription,
+  tryDemo,
+  learnMore,
+  googlepPlay,
+  appStore,
+}) {
+  return (
+    <div className="container mb-5">
+      <div className="row">
+        <div className="col-4">
+          <img src={imageURL} alt={productName} />
         </div>
-    );
+        <div className="col-2"></div>
+        <div className="col-6 p-5 mt-5">
+          <h1>{productName}</h1>
+          <p>{productDescription}</p>
+          <div>
+            <a href={tryDemo} className="btn btn-primary mx-3">
+              Try Demo
+            </a>
+            <a href={learnMore} style={{ marginLeft: "50px" }}>
+              Learn More
+            </a>
+          </div>
+          <div className="mt-3">
+            <a href={googlepPlay}>
+              <img src="media/images/googlePlayBadge.svg" alt="Google Play" />
+            </a>
+            <a href={appStore}>
+              <img
+                src="media/images/appStoreBadge.svg"
+                alt="App Store"
+                style={{ marginLeft: "50px" }}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LeftSection;
