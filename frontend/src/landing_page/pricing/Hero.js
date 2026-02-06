e409ec39a6bf2a820036d3c96b56cffe39a53cd2
Soumyaranjanrrath <soumya2005rath@gmail.com>
2026-02-06 23:04:19 +0530

    Pricing section minor change

--- frontend/src/landing_page/pricing/Hero.js
+++ frontend/src/landing_page/pricing/Hero.js
@@ -17,8 +17,8 @@ function PricingHero() {
         </div>
         <div className="col-4 p-4">
             <img src='media/images/intradayTrades.svg' alt='intraday trades' className='img-fluid mb-4' style={{maxHeight: '340px', width: 'auto'}} />
-            <h1 className="fs-3">Free intraday trades</h1>
-            <p>All intraday trades are absolutely free - 0 brokerage.</p>
+            <h1 className="fs-3">Intraday and F&O trades</h1>
+            <p>All intraday trades are charged at a flat rate of ₹20 per executed order.</p>
         </div>
         <div className="col-4 p-4">
             <img src='media/images/pricing0.svg' alt='direct mutual funds' className='img-fluid mb-4' style={{maxHeight: '340px', width: 'auto'}} />
